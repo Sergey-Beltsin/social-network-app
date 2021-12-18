e984475dd79470bc1885e461428550e517fe21bf
SergeyBeltsin <beltsins89@gmail.com>
2021-12-19 00:20:26 +0300

    add page title on auth pages

--- src/pagesLayout/login/ui.tsx
+++ src/pagesLayout/login/ui.tsx
@@ -1,13 +1,22 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import styled from "styled-components";
+import useTranslation from "next-translate/useTranslation";
 
 import { LoginForm } from "@/entities/auth/login";
 
-export const LoginPage: NextPage = () => (
-  <Container>
-    <LoginForm />
-  </Container>
-);
+export const LoginPage: NextPage = () => {
+  const { t } = useTranslation("auth");
+
+  return (
+    <Container>
+      <Head>
+        <title>{t("loginPageTitle")}</title>
+      </Head>
+      <LoginForm />
+    </Container>
+  );
+};
 
 const Container = styled.div`
   display: flex;
--- src/pagesLayout/register/ui.tsx
+++ src/pagesLayout/register/ui.tsx
@@ -1,13 +1,21 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import styled from "styled-components";
+import useTranslation from "next-translate/useTranslation";
 
 import { RegisterForm } from "@/entities/auth/register";
 
-export const RegisterPage: NextPage = () => (
-  <Container>
-    <RegisterForm />
-  </Container>
-);
+export const RegisterPage: NextPage = () => {
+  const { t } = useTranslation("auth");
+  return (
+    <Container>
+      <Head>
+        <title>{t("regPageTitle")}</title>
+      </Head>
+      <RegisterForm />
+    </Container>
+  );
+};
 
 const Container = styled.div`
   display: flex;
